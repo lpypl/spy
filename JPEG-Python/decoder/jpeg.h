bb5d47bcc712a98d97dd94a40bc9b596c71a9eb4
lpypl <lpy.henu@gmail.com>
2019-11-03 13:59:37 +0800

    读取哈夫曼表成功

---
+++ JPEG-Python/decoder/jpeg.h
@@ -0,0 +1,29 @@
+#ifndef _JPEG_H_
+#define _JPEG_H_
+
+#define SOI 0xD8
+
+// #define APP0 0xE0
+// #define APP1 0xE1
+// #define APP2 0xE2
+// #define APP3 0xE3
+// #define APP4 0xE4
+// #define APP5 0xE5
+// #define APP6 0xE6
+// #define APP7 0xE7
+// #define APP8 0xE8
+// #define APP9 0xE9
+// #define APP10 0xEA
+// #define APP11 0xEB
+// #define APP12 0xEC
+// #define APP13 0xED
+// #define APP14 0xEE
+// #define APP15 0xEF
+
+#define DQT 0xDB
+#define SOF0 0xC0
+#define DHT 0xC4
+#define SOS 0xDA
+#define EOI 0xD9
+
+#endif
