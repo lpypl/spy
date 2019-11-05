0608ef7e0086e5dfeb8f112f737f3ade62b1f6ba
lpypl <lpy.henu@gmail.com>
2019-11-05 17:30:44 +0800

    删除了一些原来测试用的信息

--- JPEG-Python/pyencoder/jpeg.py
+++ JPEG-Python/pyencoder/jpeg.py
@@ -4,38 +4,6 @@ import math
 
 from jpegTable import *
 
-# 4:2:2
-def ycrcb2sample(ycrcb):
-    """
-    对YCrCb进行取样(4:2:2)
-    :param ycrcb: origin YCrCb
-    :return: 4-Y, 2-Cr, 2-Cb
-    """
-    y = ycrcb[:, :, 0]
-    cr = ycrcb[:, 0:ycrcb.shape[1]:2, 1]
-    cb = ycrcb[:, 0:ycrcb.shape[1]:2, 2]
-    return y, cr, cb
-
-
-def sample2ycrcb(y, cr, cb):
-    """
-    将4:2:2格式的Y,Cr,Cb还原成YCrCb矩阵
-    :param y: Y
-    :param cr: Cr
-    :param cb: Cb
-    :return: YCrCb
-    """
-    newcr = np.ones((cr.shape[0], cr.shape[1]*2))
-    newcr[:, 0:newcr.shape[1]:2] = cr
-    newcr[:, 1:newcr.shape[1]:2] = cr
-
-    newcb = np.ones((cb.shape[0], cb.shape[1]*2))
-    newcb[:, 0:newcb.shape[1]:2] = cb
-    newcb[:, 1:newcb.shape[1]:2] = cb
-
-    return np.array([y, newcr, newcb]).swapaxes(0,1).swapaxes(1,2).astype(np.uint8)
-
-
 def dct(data):
     """
     dct
@@ -80,26 +48,6 @@ def data2blocks(data):
             blocks.append(data[r:r+8, c:c+8])
     return blocks, height, width
 
-
-def blocks2data(blocks, height, width):
-    """
-    将8x8 BLock list 还原成图像数据
-    :param blocks: block list
-    :param height: block data height
-    :param width: block data width
-    :return:
-    """
-    assert height%8==0 and width%8==0
-    bh = width//8
-    bv = height//8
-
-    data = np.ones((height, width), dtype=np.uint8)
-    for row in range(bv):
-        for column in range(bh):
-            data[row*8:(row+1)*8, column*8:(column+1)*8] = blocks[row*bh+column]
-
-    return data
-
 def quantifyBlock(originBlock, type):
     """
     dct并量化一个8x8 BLock
@@ -124,27 +72,6 @@ def quantifyBlock(originBlock, type):
     dct_block_quantization =  np.round((dct_block / qtable)).astype(np.int)
 
     return dct_block_quantization
-
-def inverseQuantifyBlock(originBlock, type):
-    """
-    反量化并 idct 一个 8x8 block
-    :param originBlock: 8x8 block
-    :param type: ‘H’ for luminance, 'C' for Chrominance
-    :return: 反量化并逆dct之后的 8x8 block - np.uint8
-    """
-    if type == 'L':
-        qtable = LuminanceQuantizationTable
-    elif type == 'C':
-        qtable = ChrominanceQuantizationTable
-    else:
-        raise TypeError("type must be L or C")
-
-    dct_block = (originBlock.astype(np.float32) * qtable)
-    idct_block = cv2.idct(dct_block)
-    inverse_norm_block = idct_block + 128
-    idct_block_int = inverse_norm_block.astype(np.uint8)
-
-    return idct_block_int
 
 def zigzagOrder(block):
     """
@@ -253,42 +180,7 @@ def zigzag2midSigns(zigzagBlock):
             zeroCount = 0
 
     return midSigns
-
-
-def midSigns2binaryCode(midSignsList, type):
-    """
-    zigzagBlock to mid signs
-    暂且认为若DC值为0，则只需要一个哈夫曼码，而不需要0的二进制代码
-    :param zigzagBlock: 经之字型排列之后的一个block - int list
-    :return: midSigns
-    """
-
-    if type == 'L':
-        dcTable = LuminanceDCCoefficientDifferencesTable
-        acTable = acLuminanceTable
-    elif type == 'C':
-        dcTable = ChrominanceDCCoefficientDifferencesTable
-        acTable = acChrominanceTable
-    else:
-        raise TypeError("type must be L or C")
-
-    binaryData = ""
-
-    for midSigns in midSignsList:
-
-        # DC
-        binLen, binCode = int2LenAndCode(midSigns[0])
-        binaryData += dcTable[binLen][2] + binCode
-
-        # AC
-        for sign in midSigns[1:]:
-            # 若sign[1]为0，则binCode为空字符串
-            binLen, binCode = int2LenAndCode(sign[1])
-            key = "{}/{}".format(hex(sign[0])[-1].upper(), hex(binLen)[-1].upper())
-            binaryData += acTable[key][1]
-            binaryData += binCode
-
-    return binaryData
+    
 
 def midSigns2binaryCode_Colorful(midSignsTupleList):
     """
--- JPEG-Python/pyencoder/lpy-jpeg-encoder.py
+++ JPEG-Python/pyencoder/lpy-jpeg-encoder.py
@@ -6,92 +6,6 @@ from tempfile import TemporaryDirectory
 
 npmidsignlist = []
 
-def showalways(img, title="Window Name"):
-    """
-    显示图片
-    :param img: img
-    :param title: title
-    :return:
-    """
-    cv2.imshow(title, img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-def testSampleAndDct():
-    """
-    测试子采样并量化一个图片后再逆量化并恢复图像
-    :return:
-    """
-    rgb = cv2.imread('./Pictures/Koala.bmp')
-    ycrcb = cv2.cvtColor(rgb, cv2.COLOR_RGB2YCR_CB)
-
-    y, cr, cb = ycrcb2sample(ycrcb)
-    yblocks, yh, yw = data2blocks(y)
-    crblocks, crh, crw = data2blocks(cr)
-    cbblocks, cbh, cbw = data2blocks(cb)
-
-    tyblocks = yblocks[:]
-    for i in range(len(yblocks)):
-        tyblocks[i] = quantifyBlock(yblocks[i], 'L')
-        tyblocks[i] = inverseQuantifyBlock(tyblocks[i], 'L')
-    newy = blocks2data(tyblocks, yh, yw)
-
-    tcrblocks = crblocks[:]
-    for i in range(len(crblocks)):
-        tcrblocks[i] = quantifyBlock(crblocks[i], 'C')
-        tcrblocks[i] = inverseQuantifyBlock(tcrblocks[i], 'C')
-    newcr = blocks2data(tcrblocks, crh, crw)
-
-    tcbblocks = cbblocks[:]
-    for i in range(len(cbblocks)):
-        tcbblocks[i] = quantifyBlock(cbblocks[i], 'C')
-        tcbblocks[i] = inverseQuantifyBlock(tcbblocks[i], 'C')
-    newcb = blocks2data(tcbblocks, cbh, cbw)
-
-    showalways(y)
-    showalways(newy)
-
-    newycrcb = sample2ycrcb(newy, newcr, newcb)
-    showalways(newycrcb)
-    newrgb = cv2.cvtColor(newycrcb, cv2.COLOR_YCR_CB2RGB)
-    showalways(newrgb)
-
-
-def img2jpegBinaryDataStringFile_Gray(outfile):
-    rgb = cv2.imread(outfile)
-    ycrcb = cv2.cvtColor(rgb, cv2.COLOR_RGB2YCR_CB)
-
-    # 之前考虑了采样
-    # y, cr, cb = ycrcb2sample(ycrcb)
-    yblocks, yh, yw = data2blocks(ycrcb[:, :, 0])
-    tyblocks = yblocks[:]
-    for i in range(len(yblocks)):
-        tyblocks[i] = quantifyBlock(yblocks[i], 'L')
-
-    zigList = [zigzagOrder(blk) for blk in tyblocks]
-    # npzigList = np.array(zigList)
-
-    # 差分 DC
-    diffZigList = diffBlocksDC(zigList)
-    # npdiffzigList = np.array(diffZigList)
-
-    midSignsList = []
-    for zigblock in diffZigList:
-        midSignsList.append(zigzag2midSigns(zigblock))
-
-    binaryData = midSigns2binaryCode(midSignsList, 'L')
-
-    shouldFill = (len(binaryData) + 7) // 8 * 8 - len(binaryData)
-    print("should fill is", shouldFill)
-    binaryData += '0' * shouldFill
-
-    # bin str to file
-    fout = open("files/jpeg-data-binary-string.txt", 'w')
-    fout.write(binaryData)
-    fout.close()
-
-
 def huffmanTable2BinaryDataStringFile(outfile):
     binaryData = huffmanTable2BinaryData(LuminanceDCCoefficientDifferencesTable,
                                          ChrominanceDCCoefficientDifferencesTable,
@@ -212,10 +126,6 @@ def img2jpegBinaryDataStringFile_Colorful(imname, txtname, info, SKIP_COUNT, LEA
 
     return ycrcb.shape
 
-
-import sys
-import os.path
-
 def main():
 
     PREFIX = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))+ "/"
@@ -279,8 +189,6 @@ def main():
         lpy_encoder, 'cj', huffman_bin, jpeg_data_slash_bin, jpeg_out_file, width, height, channel))
 
         print("done!")
-
-
 
 if __name__ == "__main__":
     main()
